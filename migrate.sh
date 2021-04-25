3071b5f858c20a1ebb5c28be12935ab9f14b647d
doctea <doctea@gmail.com>
2021-04-25 13:27:38 +0100

    added confirmation before importing registry, clean up temp files at start

--- migrate.sh
+++ migrate.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # Tristan ROwley 2021-04-24 -- migrates useful settings from one version of FL to another
 
 INPUTVERSION=20
 OUTPUTVERSION=21
+
+echo "Migrating settings from ${INPUTVERSION} to ${OUTPUTVERSION}"
+
+rm *export.reg
 
 reg.exe export "HKEY_CURRENT_USER\SOFTWARE\Image-Line\FL Studio ${INPUTVERSION}\Devices" devices.export.reg
 reg.exe export "HKEY_CURRENT_USER\SOFTWARE\Image-Line\FL Studio ${INPUTVERSION}\Favorite dirs" favorites.export.reg
@@ -20,4 +24,11 @@ sed -ie "s/FL Studio ${INPUTVERSION}/FL Studio ${OUTPUTVERSION}/mg" stagetwo.reg
 
 #grep -v "^Windows Registry Editor Version 5.00$" stagetwo.reg >> reimport.reg
 
-reg.exe import reimport.reg
+echo "exported files!  confirmation follows"
+
+read -p "Import regkeys now? [Y/N] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then	
+	reg.exe import reimport.reg
+	echo "OK, imported"
+fi
