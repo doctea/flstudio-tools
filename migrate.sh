78f4f77a124012cfcee8101ed26e6ec2cb6f5c3b
doctea <doctea@gmail.com>
2021-04-25 12:01:02 +0100

    remove uselesslines, add most recently used

--- migrate.sh
+++ migrate.sh
@@ -9,14 +9,15 @@ reg.exe export "HKEY_CURRENT_USER\SOFTWARE\Image-Line\FL Studio ${INPUTVERSION}\
 reg.exe export "HKEY_CURRENT_USER\SOFTWARE\Image-Line\FL Studio ${INPUTVERSION}\Favorite dirs" favorites.export.reg
 reg.exe export "HKEY_CURRENT_USER\SOFTWARE\Image-Line\FL Studio ${INPUTVERSION}\Search paths" searchpaths.export.reg
 reg.exe export "HKEY_CURRENT_USER\SOFTWARE\Image-Line\FL Studio ${INPUTVERSION}\Windows" windows.export.reg
+reg.exe export "HKEY_CURRENT_USER\SOFTWARE\Image-Line\FL Studio ${INPUTVERSION}\MRU" mru.export.reg
 
-echo "Windows Registry Editor Version 5.00" > reimport.reg
+#echo "Windows Registry Editor Version 5.00" > reimport.reg
 cat *export.reg | tr -d '\000' > stageone.reg
 
 iconv -f utf-8 -t ascii -c stageone.reg > stagetwo.reg
 
 sed -ie "s/FL Studio ${INPUTVERSION}/FL Studio ${OUTPUTVERSION}/mg" stagetwo.reg
 
-grep -v "^Windows Registry Editor Version 5.00$" stagetwo.reg >> reimport.reg
+#grep -v "^Windows Registry Editor Version 5.00$" stagetwo.reg >> reimport.reg
 
 reg.exe import reimport.reg
